3b97a118dbfb01b4ea73da23d1b6059fd9bde9a6
eranyosef2 <EranYosefHD@gmail.com>
2024-11-08 20:04:21 +0200

    Add Aminities + some css

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-
+import {amenityIcons} from '../utils/aminities'
 import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
 import { loadStay, addStayMsg } from '../store/actions/stay.actions'
 
@@ -23,25 +23,44 @@ export function StayDetails() {
       <h1>Stay Details</h1>
       {stay && (
         <div>
-          <h3>{stay.name}</h3>
-          <h4>${stay.price}</h4>
-          <pre> {stay.name} </pre>
+          <div className="stay-header">
+            <h3>{stay.name}</h3>
+            <h4>${stay.price}</h4>
+            <pre> {stay.name} </pre>
+          </div>
+
           <div className="stay-gallery">
             {stay.imgUrls.map((url, index) => (
-              <img key={index} src={url} alt={stay.name} className="stay-image" />
+              <div key={index} className="image-wrapper">
+                <img src={url} alt={stay.name} className="stay-image" />
+              </div>
             ))}
           </div>
+
           <div className="stay-description">
             <p>{stay.summary}</p>
             <div className="stay-capacity">
-            <p>Capacity: {stay.capacity} guests</p>
+              <p> {stay.capacity} guests • 2 bedrooms • 3 beds • 2 baths</p>
+            </div>
           </div>
-          </div>
+
           <div className="stay-host">
             <img src={stay.host.imgUrl} alt={stay.host.fullname} className="host-image" />
             <div className="host-details">
               <p>Hosted by {stay.host.fullname}</p>
             </div>
+          </div>
+
+          <div className="stay-amenities">
+            <h3>What this place offers</h3>
+            <ul>
+              {stay.amenities.map((amenity, index) => (
+                <li key={index} className="amenity-item">
+                  <img src={amenityIcons[amenity]} alt={amenity} className="amenity-icon" />
+                  <span>{amenity}</span>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       )}
