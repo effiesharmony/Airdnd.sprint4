ca11f6179be28a6a32a22fb7d8945b55acb6c92a
Effie Armony <harmony.effie@gmail.com>
2024-11-10 00:58:08 +0200

    Categories bar main functionality + image carousel & style

--- src/cmps/ImageCarousel.jsx
+++ src/cmps/ImageCarousel.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 export function ImageCarousel({ stay, onShowDetails }) {
-    const [imgIndex, setImgIndex] = useState(0);
+    const [imgIndex, setImgIndex] = useState(0)
 
     function onHandleImgChange(change) {
-        const newIndex = imgIndex + change;
+        const newIndex = imgIndex + change
         if (newIndex >= 0 && newIndex < stay.imgUrls.length) {
-            setImgIndex(newIndex);
+            setImgIndex(newIndex)
         }
     }
 
@@ -14,8 +14,13 @@ export function ImageCarousel({ stay, onShowDetails }) {
         <div className="carousel-container">
             <div className="image-container">
                 {imgIndex > 0 && (
-                    <button className="nav-button prev" onClick={() => onHandleImgChange(-1)}>❮</button>
+                    <button className="nav-button prev" onClick={() => onHandleImgChange(-1)}>
+                        <img src="public/svg/leftArrow.svg" alt="Left arrow" />
+                    </button>
                 )}
+                <div className="heart">
+                    <img src="public/svg/heart.svg" alt="Heart" />
+                    </div>
                 <img onClick={() => onShowDetails(stay._id)} src={stay.imgUrls[imgIndex]} alt="Stay Image" />
                 <div className="dots">
                     {stay.imgUrls.map((_, index) => (
@@ -23,9 +28,11 @@ export function ImageCarousel({ stay, onShowDetails }) {
                     ))}
                 </div>
                 {imgIndex < stay.imgUrls.length - 1 && (
-                    <button className="nav-button next" onClick={() => onHandleImgChange(1)}>❯</button>
+                    <button className="nav-button next" onClick={() => onHandleImgChange(1)}>
+                        <img src="public/svg/rightArrow.svg" alt="Right arrow" />
+                    </button>
                 )}
             </div>
         </div>
-    );
+    )
 }
