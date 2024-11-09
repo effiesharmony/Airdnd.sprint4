124eb722097b70654b7d63061a30eee7fb6554c1
Effie Armony <harmony.effie@gmail.com>
2024-11-09 18:09:05 +0200

    Finish list & preview & update gitignore

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -1,32 +1,28 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { ImageCarousel } from './ImageCarousel.jsx'
+import { getReviewAvg, numberWithCommas } from '../services/util.service.js'
 
 export function StayPreview({ stay }) {
-
-    // function StayDetails() {
-    //     const { stayId } = useParams()
-    //     // const stay = useSelector(storeState => storeState.stayModule.stay)
-    //     useEffect(() => {
-    //         loadStay(stayId)
-    //     }, [stayId])
-    // }
     const navigate = useNavigate()
 
     function onShowDetails(stayId) {
         navigate(`/stay/${stayId}`)
     }
 
-    console.log(stay._id)
     return (
         <section className="stay-preview-container">
             <div className="imgCarousel">
                 <ImageCarousel stay={stay} onShowDetails={onShowDetails} />
             </div>
             <article onClick={() => onShowDetails(stay._id)} className="stay-preview">
-                <p>{stay.name}</p>
-                how far away from current location
-                <p>{stay.availableDates[0].month} {stay.availableDates[0].start} - {stay.availableDates[0].end}</p>
-                <p>${stay.price} night</p>
+                <p className="stay-loc">{stay.loc.city}, {stay.loc.country}</p>
+                <p className="stay-distance">4,019 kilometers away</p>
+                <p className="stay-dates">{stay.availableDates[0].month} {stay.availableDates[0].start} - {stay.availableDates[0].end}</p>
+                <p className="stay-price"><span>${numberWithCommas(stay.price)}</span> night</p>
+                <div className="stay-rating-container">
+                    <img className="star" src="public/svg/star.svg" alt="Star" />
+                    <p className="rating-num">{getReviewAvg(stay.reviews).toFixed(1)}</p>
+                </div>
 
             </article>
         </section>
