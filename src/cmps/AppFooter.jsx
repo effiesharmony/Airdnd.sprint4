902cca6c03b31f78337b7df611c194a413c69a17
Valery Vishnevezky <valery020201@gmail.com>
2024-11-10 02:15:08 +0200

    Add a footer

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,16 +1,31 @@
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 
 export function AppFooter() {
-	const count = useSelector(storeState => storeState.userModule.count)
+  const count = useSelector((storeState) => storeState.userModule.count);
 
-	return (
-		<footer className="app-footer full">
-			<p>Coffeerights &copy; 2024</p>
-			<p>Count: {count}</p>
-            
-            {import.meta.env.VITE_LOCAL ? 
-                <span className="local-services">Local Services</span> : 
-                <span className="remote-services">Remote Services</span>}
-		</footer>
-	)
+  return (
+    <footer className="app-footer full">
+      <div class="footer-content">
+        <p>© 2024 Airbnb, Inc.</p>
+        <div class="footer-links">
+          <a href="/">Terms</a>
+          <a href="/">Sitemap</a>
+          <a href="/">Privacy</a>
+          <a href="/">Your Privacy Choices</a>
+        </div>
+      </div>
+
+      <div class="footer-content">
+		<div class="footer-lng">
+        <img
+          className="app-footer-world-svg"
+          src="/public/svg/world.svg"
+          alt=""
+        />
+        <h1>English (US)</h1>
+		</div>
+        <h1>₪ ILS</h1>
+      </div>
+    </footer>
+  );
 }
