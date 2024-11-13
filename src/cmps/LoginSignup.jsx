90c303732d69d62613c9d9a2aa03e779e987780a
Effie Armony <harmony.effie@gmail.com>
2024-11-13 11:25:37 +0200

    Login signup testing

--- src/cmps/LoginSignup.jsx
+++ src/cmps/LoginSignup.jsx
@@ -4,11 +4,13 @@ import { NavLink } from 'react-router-dom'
 export function LoginSignup() {
     return (
         <div className="login-page">
+            <div className="login-form-container">
             <nav>
                 <NavLink to=".">Login</NavLink>
                 <NavLink to="signup">Signup</NavLink>
             </nav>
             <Outlet/>
+            </div>
         </div>
     )
 }
