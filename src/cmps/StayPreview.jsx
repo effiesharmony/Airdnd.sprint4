9f23bf0ebde1b7a7bb26c814e01fec0fb6a6c9c5
Effie Armony <harmony.effie@gmail.com>
2024-11-17 10:29:03 +0200

    Add default rating until we fix demo-data

--- src/cmps/StayPreview.jsx
+++ src/cmps/StayPreview.jsx
@@ -21,7 +21,8 @@ export function StayPreview({ stay }) {
                 <p className="stay-price"><span>${numberWithCommas(stay.price)}</span> night</p>
                 <div className="stay-rating-container">
                     <img className="star" src="public/svg/star.svg" alt="Star" />
-                    <p className="rating-num">{getReviewAvg(stay.reviews)}</p>
+                    <p className="rating-num">5.0</p>
+                    {/* <p className="rating-num">{getReviewAvg(stay.reviews)}</p> */}
                     <p className="ratings-amount">({stay.reviews.length})</p>
                 </div>
 
