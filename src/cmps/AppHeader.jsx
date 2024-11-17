6096fcdbddbf3b6b718cbad8625f44018b1d59f8
Effie Armony <harmony.effie@gmail.com>
2024-11-17 10:28:36 +0200

    Adjust padding for layout

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react"
 import { AppHeaderFull } from "./AppHeaderFull"
 import { AppHeaderMobile } from "./AppHeaderMobile"
-import { handleMobileResize } from "../services/utils/util.service.js"
 
 export function AppHeader() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 745)
 
   useEffect(() => {
     window.addEventListener("resize", handleMobileResize)
+
+    function handleMobileResize() {
+      setIsMobile(window.innerWidth < 745)
+    }
 
     return () => {
       window.removeEventListener("resize", handleMobileResize)
