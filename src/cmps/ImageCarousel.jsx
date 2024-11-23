e473304837e1e1d611e0acb66fd377536280e873
effiesharmony <harmony.effie@gmail.com>
2024-11-23 20:41:50 +0200

    Add heart toggle for wishlist

--- src/cmps/ImageCarousel.jsx
+++ src/cmps/ImageCarousel.jsx
@@ -1,8 +1,13 @@
 import { useState, useEffect } from 'react'
+import { storageService } from '../services/async-storage.service'
+import { useDispatch, useSelector } from 'react-redux'
 
 export function ImageCarousel({ stay, onShowDetails }) {
+    const dispatch = useDispatch()
+    const likedStays = useSelector((state) => state.stayModule.LikedStays)
     const [imgIndex, setImgIndex] = useState(0)
     const [isMobile, setIsMobile] = useState(window.innerWidth < 745)
+    const [isLiked, setIsLiked] = useState(likedStays.includes(stay._id))
 
     useEffect(() => {
         window.addEventListener("resize", handleMobileResize)
@@ -19,17 +24,25 @@ export function ImageCarousel({ stay, onShowDetails }) {
     function showPrevImg(ev) {
         ev.stopPropagation()
         if (imgIndex === 0) return
-        setImgIndex(index => {
-            return imgIndex - 1
-        })
+        setImgIndex(imgIndex - 1)
     }
 
     function showNextImg(ev) {
         ev.stopPropagation()
         if (imgIndex === stay.imgUrls.length - 1) return
-        setImgIndex(index => {
-            return imgIndex + 1
-        })
+        setImgIndex(imgIndex + 1)
+
+    }
+
+    function toggleHeart() {
+        const updatedLikedStays = isLiked
+            ? likedStays.filter(stayId => stayId !== stay._id)
+            : [...likedStays, stay._id];
+
+        dispatch({ type: 'SET_LIKED_STAYS', LikedStays: updatedLikedStays })
+        setIsLiked(!isLiked);
+        localStorage.setItem('likedStays', JSON.stringify(updatedLikedStays))
+
     }
 
     return (
@@ -40,15 +53,14 @@ export function ImageCarousel({ stay, onShowDetails }) {
                         <img src="public/svg/leftArrow.svg" alt="Left arrow" />
                     </button>
                 )}
-                <div className="heart">
-                    <img src="public/svg/heart.svg" alt="Heart" />
+                <div className="heart" onClick={toggleHeart}>
+                    <img src={isLiked ? 'public/svg/redHeart.svg' : 'public/svg/heart.svg'} alt="Heart" />
                 </div>
                 <div className="images">
                     {stay.imgUrls.map((url, index) => (
                         <img id={index} key={index} onClick={() => onShowDetails(stay._id)} src={url} style={{ translate: `${-100 * imgIndex}%` }} />
                     ))}
                 </div>
-                {/* <img onClick={() => onShowDetails(stay._id)} src={stay.imgUrls[imgIndex]} alt="Stay Image" /> */}
                 <div className="dots">
                     {stay.imgUrls.map((_, index) => (
                         <span key={index} className={`dot ${index === imgIndex ? 'active' : ''}`}></span>
